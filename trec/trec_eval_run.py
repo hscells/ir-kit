3c2ee85d5eecb565811c77ddf27d96ecb31b3b20
Harry Scells <harryscells@gmail.com>
2017-05-12 16:38:22 +1000

    Add query num parameter to constructor

--- trec/trec_eval_run.py
+++ trec/trec_eval_run.py
@@ -6,12 +6,13 @@ May 2017
 
 
 class TrecEvalRun(object):
-    def __init__(self, topic: str, doc_id: str, rank: int, score: float, run_id: str):
-        self.run_id = run_id
-        self.score = score
-        self.rank = rank
-        self.doc_id = doc_id
+    def __init__(self, topic: str, q: int, doc_id: str, rank: int, score: float, run_id: str):
         self.topic = topic
+        self.q = q
+        self.doc_id = doc_id
+        self.rank = rank
+        self.score = score
+        self.run_id = run_id
 
     def __str__(self):
         return '{}\t{}\t{}\t{}\t{}'.format(self.topic, self.doc_id, self.rank, self.score,
