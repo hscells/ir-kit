cdb446ad8c01464888fb998413f9ad5985876f98
Harry Scells <harryscells@gmail.com>
2017-06-12 14:42:18 +1000

    Update git location.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/hscells/irkit',
+    url='https://github.com/hscells/ir-kit',
 
     # Author details
     author='Harry Scells',
